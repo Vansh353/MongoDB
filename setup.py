2079d72e724e090d5f9d694126e96679e9ce7d22
Vansh353 <vansh.malhotra@seaflux.tech>
2024-05-13 11:12:35 +0530

    changes in setu.py

--- setup.py
+++ setup.py
@@ -1,17 +1,7 @@
 from setuptools import setup, find_packages
 from typing import List
 
-HYPEN_E_DOT='-e .'
 
-def get_requiremet(file_path:str)->List[str]:
-    requirements = []
-    with open(file_path) as f:
-        requirements=f.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
-
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-    return requirements
 
 
 with open('README.md', 'r', encoding='utf-8') as f:
@@ -38,5 +28,5 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires=get_requiremet("./requirements_dev.txt"),
+   
 )
