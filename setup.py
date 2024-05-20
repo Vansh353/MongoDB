c23ea51c00e723a5fe4f8a422cfb25e653b63138
Vansh353 <vansh.malhotra@seaflux.tech>
2024-05-20 12:55:38 +0530

    setupy.py  updated

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
 
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 REPO_NAME = "MongoDB"
 PKG_NAME= "mongo_bridge"
 AUTHOR_USER_NAME = "Vansh353"
